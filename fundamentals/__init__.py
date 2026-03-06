c6e33619bec8ca04d54f101a25bf848cf5e92806
Dieter J Kybelksties <github@kybelksties.com>
2026-03-06 19:25:12 +0000

    auto-committed by release-to-pypi.py

--- fundamentals/__init__.py
+++ fundamentals/__init__.py
@@ -1,9 +1,9 @@
-from .exceptions import *
-from .string_utils import *
 from .basic_functions import *
+from .exceptions import *
 from .bash import *
-from .extended_enum import *
 from .overrides import *
+from .extended_enum import *
+from .string_utils import *
 from .thread_with_return import *
 
-__version__ = "0.1.2"
+__version__ = "0.2.0"
